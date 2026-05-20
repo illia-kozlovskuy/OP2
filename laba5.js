42f7221c851de4626e907009445a544a4f311c29
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 09:58:39 +0300

    Lab5-4

--- laba5.js
+++ laba5.js
@@ -1,40 +1,75 @@
 function mapSync(array, fn) {
-    const result = [];
+  const result = [];
 
-    for (let i = 0; i < array.length; i++) {
-        result.push(fn(array[i], i));
-    }
+  for (let i = 0; i < array.length; i++) {
+    result.push(fn(array[i], i));
+  }
 
-    return result;
+  return result;
 }
 function mapCallback(array, fn, cb) {
+  let i = 0;
+  const result = [];
+
+  function next() {
+    if (i >= array.length) {
+      return cb(null, result);
+    }
+
+    setTimeout(() => {
+      if (signal?.aborted) {
+        return reject(new Error("AbortError"));
+      }
+      try {
+        result.push(fn(array[i], i));
+        i++;
+        next();
+      } catch (err) {
+        cb(err, null);
+      }
+    }, 100);
+  }
+
+  next();
+}
+function mapPromise(array, fn) {
+  return new Promise((resolve, reject) => {
     let i = 0;
     const result = [];
 
     function next() {
-        if (i >= array.length) {
-            return cb(null, result);
-        }
+      if (i >= array.length) {
+        return resolve(result);
+      }
 
-        setTimeout(() => {
-            try {
-                result.push(fn(array[i], i));
-                i++;
-                next();
-            } catch (err) {
-                cb(err, null);
-            }
-        }, 100);
+      setTimeout(() => {
+        try {
+          result.push(fn(array[i], i));
+          i++;
+          next();
+        } catch (err) {
+          reject(err);
+        }
+      }, 100);
     }
 
     next();
+  });
 }
-function mapPromise(array, fn) {
-    return new Promise((resolve, reject) => {
+function mapAbortable(array, fn) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const promise = new Promise((resolve, reject) => {
         let i = 0;
         const result = [];
 
         function next() {
+
+            if (signal.aborted) {
+                return reject(new Error("AbortError"));
+            }
+
             if (i >= array.length) {
                 return resolve(result);
             }
@@ -50,6 +85,12 @@ function mapPromise(array, fn) {
             }, 100);
         }
 
+        signal.addEventListener("abort", () => {
+            reject(new Error("AbortError"));
+        });
+
         next();
     });
+
+    return { promise, controller };
 }
