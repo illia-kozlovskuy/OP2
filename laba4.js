3a7c9d69e238d1814bd9cbee557cdf90b769a4d8
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 09:30:24 +0300

    Lab4-4

--- laba4.js
+++ laba4.js
@@ -15,3 +15,20 @@ export class BiPriorityQueue {
   peek(type = "highest") {}
   dequeue(type = "highest") {}
 }
+peek(type = "highest") {
+    if (this.items.length === 0) return null;
+
+    let target = this.items[0];
+
+    for (const item of this.items) {
+        if (type === "oldest" && item.order < target.order) {
+            target = item;
+        }
+
+        if (type === "newest" && item.order > target.order) {
+            target = item;
+        }
+    }
+
+    return target.value;
+}
