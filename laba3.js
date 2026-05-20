9da1b5c1855c182504244f7105bd341b47b8a0af
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 09:00:10 +0300

    Lab3-8

--- laba3.js
+++ laba3.js
@@ -1,40 +1,63 @@
-export function memoize(fn) {
-  const cache = new Map();
+export function memoize(fn, options = {}) {
+    const {
+        maxSize = Infinity,
+        ttl = null,
+        customEvict = null
+    } = options;
 
-  return function (...args) {
-    const key = JSON.stringify(args);
+    const cache = new Map();
 
-    if (cache.has(key)) {
-      const item = cache.get(key);
-
-      item.lastAccess = Date.now();
-      item.freq++;
-
-      cache.delete(key);
-      cache.set(key, item);
-
-      if (isExpired(item, ttl)) {
-        cache.delete(key);
-      } else {
-        return item.value;
-      }
+    function isExpired(item) {
+        return ttl && (Date.now() - item.createdAt > ttl);
     }
-    const result = fn.apply(this, args);
 
-    cache.set(key, {
-      value: result,
-      createdAt: Date.now(),
-      lastAccess: Date.now(),
-      freq: 1,
-    });
+    function evictLFU() {
+        let minFreq = Infinity;
+        let keyToDelete = null;
 
-    return result;
-  };
-}
-if (cache.size > maxSize) {
-    const firstKey = cache.keys().next().value;
-    cache.delete(firstKey);
-}
-if (customEvict) {
-    customEvict(cache);
+        for (const [key, value] of cache.entries()) {
+            if (value.freq < minFreq) {
+                minFreq = value.freq;
+                keyToDelete = key;
+            }
+        }
+
+        cache.delete(keyToDelete);
+    }
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            const item = cache.get(key);
+
+            if (isExpired(item)) {
+                cache.delete(key);
+            } else {
+                item.freq++;
+                item.lastAccess = Date.now();
+
+                cache.delete(key);
+                cache.set(key, item);
+
+                return item.value;
+            }
+        }
+
+        const result = fn.apply(this, args);
+
+        cache.set(key, {
+            value: result,
+            createdAt: Date.now(),
+            lastAccess: Date.now(),
+            freq: 1
+        });
+
+        if (cache.size > maxSize) {
+            if (customEvict) customEvict(cache);
+            else evictLFU();
+        }
+
+        return result;
+    };
 }
