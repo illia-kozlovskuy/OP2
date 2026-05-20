accb0e7d94683f8c4fff762dd963c086d45d6a71
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 09:35:17 +0300

    Lab4-6

--- laba4.js
+++ laba4.js
@@ -6,47 +6,52 @@ export class BiPriorityQueue {
 
   enqueue(value, priority) {
     this.items.push({
-        value,
-        priority,
-        order: this.counter++
+      value,
+      priority,
+      order: this.counter++,
     });
-}
-
-  peek(type = "highest") {}
-  dequeue(type = "highest") {}
-}
-peek(type = "highest") {
+  }
+  peek(type = "highest") {
     if (this.items.length === 0) return null;
 
     let target = this.items[0];
 
     for (const item of this.items) {
-        if (type === "oldest" && item.order < target.order) {
-            target = item;
-        }
+      if (type === "highest" && item.priority > target.priority) {
+        target = item;
+      }
 
-        if (type === "newest" && item.order > target.order) {
-            target = item;
-        }
+      if (type === "lowest" && item.priority < target.priority) {
+        target = item;
+      }
+
+      if (type === "oldest" && item.order < target.order) {
+        target = item;
+      }
+
+      if (type === "newest" && item.order > target.order) {
+        target = item;
+      }
     }
 
     return target.value;
-}
-dequeue(type = "highest") {
+  }
+  dequeue(type = "highest") {
     if (this.items.length === 0) return null;
 
     let index = 0;
 
     for (let i = 1; i < this.items.length; i++) {
-        const a = this.items[i];
-        const b = this.items[index];
+      const a = this.items[i];
+      const b = this.items[index];
 
-        if (type === "highest" && a.priority > b.priority) index = i;
-        if (type === "lowest" && a.priority < b.priority) index = i;
-        if (type === "oldest" && a.order < b.order) index = i;
-        if (type === "newest" && a.order > b.order) index = i;
+      if (type === "highest" && a.priority > b.priority) index = i;
+      if (type === "lowest" && a.priority < b.priority) index = i;
+      if (type === "oldest" && a.order < b.order) index = i;
+      if (type === "newest" && a.order > b.order) index = i;
     }
 
     const [removed] = this.items.splice(index, 1);
     return removed.value;
+  }
 }
