802583a19c8b3c40e510563feaebe1fcec28657f
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 08:21:47 +0300

    Lab1-1

---
+++ laba1.js
@@ -0,0 +1,23 @@
+function* fibonacciGenerator() {
+    let a = 0, b = 1;
+
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+function consumeWithTimeout(iterator, seconds) {
+    const start = Date.now();
+
+    const interval = setInterval(() => {
+
+        if (Date.now() - start >= seconds * 1000) {
+            clearInterval(interval);
+            return;
+        }
+
+        const value = iterator.next().value;
+        console.log(value);
+
+    }, 500);
+}
