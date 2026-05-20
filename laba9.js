30a54f92c0f3e91ba9f073bdee60642642568763
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 11:47:07 +0300

    Lab9-ok

--- laba9.js
+++ laba9.js
@@ -1,14 +1,25 @@
 function log(level) {
   return function (fn) {
-    return function (...args) {
+    return async function (...args) {
       const time = new Date().toISOString();
-
-      const result = fn(...args);
-
-      console.log(`[${time}] [${level}] args:`, args);
-      console.log(`[${time}] [${level}] result:`, result);
-
-      return result;
+      try {
+        const result = await fn(...args);
+        if (level === "Error") {
+          console.log(`[${time}] [${level}] args:`, args);
+          console.log(`[${time}] [${level}] result:`, result);
+        }
+        return result;
+      } catch (err) {
+        if (level === "Error") {
+          console.log(`[${time}] [Error] args:`, args);
+          console.log(`[${time}] [Error] error:`, err.message);
+        }
+        throw err;
+      }
     };
   };
 }
+const add = log ("Info")(async (a, b) => {
+    return a + b;
+});
+add(5, 7).then(res =>console.log(res));
