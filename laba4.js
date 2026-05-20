a9a5edb614abc2ce431d01aef6467fd5faa1ce65
illia-kozlovskuy <lordone654@gmail.com>
2026-05-20 09:32:22 +0300

    Lab4-5

--- laba4.js
+++ laba4.js
@@ -32,3 +32,21 @@ peek(type = "highest") {
 
     return target.value;
 }
+dequeue(type = "highest") {
+    if (this.items.length === 0) return null;
+
+    let index = 0;
+
+    for (let i = 1; i < this.items.length; i++) {
+        const a = this.items[i];
+        const b = this.items[index];
+
+        if (type === "highest" && a.priority > b.priority) index = i;
+        if (type === "lowest" && a.priority < b.priority) index = i;
+        if (type === "oldest" && a.order < b.order) index = i;
+        if (type === "newest" && a.order > b.order) index = i;
+    }
+
+    const [removed] = this.items.splice(index, 1);
+    return removed.value;
+}
